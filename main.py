b545dc5f51b58543737cea8fcac46ce0a251b222
CursedDomino <dillon.daughrity@outlook.com>
2024-02-06 09:33:03 -0600

    Test

--- main.py
+++ main.py
@@ -1,7 +1,24 @@
-#REMOVE PASS AND FIX THIS FUNCTION
 def palindrome(word):
-    pass
+    count = 1
+
+    if word.isspace():
+        return False
+    
+    word = word.upper()
+    
+    word = word.replace(' ', '')
+
+    for letter in word:
+        if letter == ' ':
+            continue
+        elif letter == word[len(word) - count]:
+            count += 1
+            continue
+        else:
+            return False
+    return True
+
+word = 'kayak'
 
 if __name__ == '__main__': 
-    #REMOVE PASS AND YOUR CODE GOES HERE
-    pass
+    print(palindrome(word))
